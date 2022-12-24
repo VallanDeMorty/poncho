f24b2c73111645865ef4483c05237a57e995e590
Andrew Malikov <andrew.devels@gmail.com>
2022-12-24 14:06:15 +0400

    build: add a script to build self-executable

---
+++ build/build-self-executable.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+dotnet publish ./Poncho.Cli/Poncho.Cli.fsproj \
+    --configuration Release \
+    --framework net7.0 \
+    --output ./app \
+    --self-contained True \
+    --runtime osx.11.0-x64 \
+    --verbosity Normal \
+    /property:PublishTrimmed=True \
+    /property:PublishSingleFile=True \
+    /property:IncludeNativeLibrariesForSelfExtract=True \
+    /property:DebugType=None \
+    /property:DebugSymbols=False
